1105a39774fd5520dbec999eaa2cc9614138d7c8
Aarni Koskela <akx@iki.fi>
2021-06-20 23:20:05 +0300

    Become django-form-designer-kcs 1.1.0

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='django-form-designer-ai',
-    version='1.0.1',
-    url='http://github.com/andersinno/django-form-designer-ai',
+    name='django-form-designer-kcs',
+    version='1.1.0',
+    url='http://github.com/kcsry/django-form-designer',
     license='BSD',
-    maintainer='Anders Innovations Ltd',
-    maintainer_email='info@anders.fi',
     packages=find_packages('.', include=[
         'form_designer',
         'form_designer.*',
